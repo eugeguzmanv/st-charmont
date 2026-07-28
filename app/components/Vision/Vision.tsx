d22439b1ef0918f6a7688f477451c61e67b03d8d
Eugenio Guzman <eugenio.guzman@hp.com>
2026-07-27 18:53:14 -0600

    final removal

--- app/components/Vision/Vision.tsx
+++ app/components/Vision/Vision.tsx
@@ -6,7 +6,6 @@ export default function Vision() {
       <div className={styles.inner}>
         
         <h2 className={styles.heading}>UNA VISIÓN EXTRAORDINARIA</h2>
-        <p className={styles.subheading}>Más que un lugar. Un estilo de vida.</p>
         <p className={styles.body}>
         St. Charmont nace con la intención de crear uno de los destinos residenciales y de hospitalidad más exclusivos de Yucatán. Un entorno donde la arquitectura, la naturaleza, el bienestar y el servicio convergen para ofrecer una experiencia única, diseñada para disfrutarse generación tras generación.
         </p>
